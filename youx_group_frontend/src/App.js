6330f2356edc9b32ccbbc63e89692bf0c9e2773e
Loris Lambert <lohane1.8gd@gmail.com>
2021-09-25 15:56:23 -0300

    Subindo nova versão do front depois das 0h do dia 24/09

--- youx_group_frontend/src/App.js
+++ youx_group_frontend/src/App.js
@@ -1,18 +1,10 @@
-import React from 'react';
-import './App.css';
-import Cabecalho from './components/Cabecalho';
-import Rodape from './components/Rodepe';
-import Login from './containers/Login/Login';
+import React from 'react'
+
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <Cabecalho/>
-      <div className="Container">
-        <h1>Bem-vindo!</h1>
-        <Login />
-      </div>
-      <Rodape/>
     </div>
   );
 }
---
+++ youx_group_frontend/src/components/Botao/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function index(props) {
+    return (
+      <button>
+        <p>{props.title}</p>
+      </button>
+    )
+  }
