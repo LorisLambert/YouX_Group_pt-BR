1e7fbad5d4ff598e23da8b5f9c4ffcb7bfb58060
Loris Lambert <lohane1.8gd@gmail.com>
2021-09-24 23:48:12 -0300

    Subindo a primeira versão da aplicação antes das 0h do dia 24/09

---
+++ youxgroupbackend/src/test/java/com/lorislambert/youxgroupbackend/YouxgroupbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorislambert.youxgroupbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouxgroupbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
